2531ada23510f754ed3068c9d1c693a38eb70598
ghenima-ouldahmed <112975120+ghenima-ouldahmed@users.noreply.github.com>
2022-10-24 15:48:02 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,17 @@
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        // Question1
+        // Question1 l'interface fonctionnelle Somme 
         Somme<Integer> sommeInt = (x, y) -> x + y;
 
         Somme<Double> sommeDouble = (x, y) -> x + y;
         Somme<Long> sommeLong = (x, y) -> x + y;
         Somme<String> sommeString = (x, y) -> x.concat(y);
 
-        // Question2
+        // Question2 l'interface fonctionnelle ToString générique sur un type T 
         List<String> l2s = new ArrayList<>();
         l2s.add("e1");
         l2s.add("e2");
